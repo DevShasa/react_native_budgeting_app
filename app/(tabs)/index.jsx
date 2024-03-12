4ab889f7213e7784600ff8f586448f94d3e82fc2
DevShasa <shasa.thuo@gmail.com>
2024-03-12 23:33:01 +0300

    feat: connected to supabase and got data

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,53 +1,66 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import { Link, useRouter } from 'expo-router'
-import services from '../../utils/services' 
-import { client } from '../../utils/KindeConfig'
-
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Link, useRouter } from "expo-router";
+import services from "../../utils/services";
+import { client } from "../../utils/KindeConfig";
+import { supabase } from "../../utils/subaBaseConfig";
 
 export default function Home() {
+	const router = useRouter();
 
-    const router = useRouter()
+	/**
+	 * Check if user is authenticated
+	 */
+	const checkUserAuth = async () => {
+		const result = await services.getData("login");
 
-    /**
-     * Check if user is authenticated
-     */
-    const checkUserAuth = async()=>{
-        const result = await services.getData("login")
-        services
-        if(!result){
-            // redirect to login page
-            router.replace("/login")
-        }
-        console.log("LOG IN --->",result)
-    }
+		services;
+		if (!result) {
+			// redirect to login page
+			router.replace("/login");
+		}
+		console.log("LOG IN --->", result);
+	};
 
+	// Fetch all categories that belong to a specific user
+	const getUsersCategory = async () => {
+		const user = await client.getUserDetails();
+        console.log(user.email)
 
-    const handleLogout = async()=>{
-      const loggedOut = await client.logout()
-      if(loggedOut){
-        await services.clearItems()
-        router.replace("/login")
-      }
-    }
+        let { data: Category, error } = await supabase
+        .from('Category')
+        .select('*')        
+		.eq("created_by", user.email);
 
-    useEffect(()=>{
-        checkUserAuth()
-    },[])
+		console.log("Users category --->", Category);
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Guruji my maan</Text>
-      <Button title="Logout" onPress={handleLogout} />
-    </View>
-  )
+	const handleLogout = async () => {
+		const loggedOut = await client.logout();
+		if (loggedOut) {
+			await services.clearItems();
+			router.replace("/login");
+		}
+	};
+
+	useEffect(() => {
+		checkUserAuth();
+        getUsersCategory()
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Guruji my maan</Text>
+			<Button title="Logout" onPress={handleLogout} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container:{
-    //   marginTop: 20
-    },
-    text:{
-        fontSize: 20,
-    }
-})
+	container: {
+		marginTop: 20,
+	},
+	text: {
+		fontSize: 20,
+	},
+});
